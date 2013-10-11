73ec3e731462017335905995f5f48975c32fe404
Ed Solis <edsfocci@gmail.com>
2013-10-10 18:31:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdsFociCom::Application.config.secret_token = 'da471ea79fd4f7ffbae806da961290bdd1837d3fa91c11cfbd4fa5289b9aed9ed0311b9f2ee197e844d24daaba7fc7fb9b4a2fe7a9ce8b66bef0cf5d2521a6bd'
